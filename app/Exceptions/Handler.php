acf6ed2e10734f6be71011f5896451b6bf7a2c32
trannguyenhan <trannguyenhan01092000@gmail.com>
2022-09-15 20:14:10 +0700

    add handler exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +38,18 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (UnauthorizedException $e, $request){
+            return response()->json([
+                'message' => 'Không có quyền'
+            ], 403);
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->json([
+                'message' => 'Không có API này'
+            ], 404);
         });
     }
 }
