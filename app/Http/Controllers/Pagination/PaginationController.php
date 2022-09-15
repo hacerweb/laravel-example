0213e87f076cd9f6e332266cd136320ff99e4535
trannguyenhan <trannguyenhan01092000@gmail.com>
2022-09-15 15:42:30 +0700

    add api pagination usre

--- app/Http/Controllers/Pagination/PaginationController.php
+++ app/Http/Controllers/Pagination/PaginationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Pagination;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,7 +11,28 @@ class PaginationController extends Controller
 {
     public function index()
     {
-        $users = DB::table('users')->paginate(3);
+        $users = DB::table('users')->simplePaginate(3);
         return view('pagination.user', ['users' => $users]);
+    }
+
+    public function indexApi(Request $request){
+        $perPage = $request->query('per_page');
+        $page = $request->query('page');
+
+        if($perPage == null){
+            $perPage = 10;
+        }
+
+        if($page == null){
+            $page = 1;
+        }
+
+        $count = User::query()->count();
+        $data = User::query()->take($perPage)->skip(($page - 1) * $perPage)->get();
+
+        return response()->json([
+            'total' => $count,
+            'data' => $data
+        ]);
     }
 }
